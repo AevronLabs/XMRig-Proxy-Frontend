29f27170d91bc0cc68c51e53ec08920f3a8ac645
BukkitEventListener <103391432+QXIoa@users.noreply.github.com>
2025-12-10 20:41:42 -0500

    Update proxy configuration and credentials

--- php/config.php
+++ php/config.php
@@ -1,11 +1,12 @@
 <?php
 //-- Webapp password
-$app_password = "SECRET";
+$app_password = "XXXX";
 
 //-- Array of proxies ip adresse, port ,token and label
 $proxy_list = array();
-$proxy_list[0] = array("ip"=>"127.0.0.1", "port"=>"8000", "token"=>"SECRET", "label"=>"PROXY 1 LABEL");
+$proxy_list[0] = array("ip"=>"127.0.0.1", "port"=>"6789", "token"=>"haha", "label"=>"LolPool");
 
 $max_history = 150; //-- max history records per proxy for json file
 $max_history_days = 180; //-- max days of graph history
+
 ?>
