fe0975ead4407eb46ac6fa2e9b4ecf24c0131d65
BukkitEventListener <103391432+QXIoa@users.noreply.github.com>
2025-12-13 16:09:18 -0500

    Reverse 834534bb9873b9fdc5fb2053330e74f192b1265c

--- php/config.php
+++ php/config.php
@@ -3,10 +3,10 @@
 $app_password = "XXXX";
 
 $proxy_list = array();
-$proxy_list[0] = array("ip"=>"127.0.0.1", "port"=>"6789", "token"=>"haha", "label"=>"LolPool CPU");
-$proxy_list[1] = array("ip"=>"127.0.0.1", "port"=>"6790", "token"=>"haha", "label"=>"LolPool GPU");
+$proxy_list[0] = array("ip"=>"127.0.0.1", "port"=>"6789", "token"=>"haha", "label"=>"LolPool");
 $max_history = 150; //-- max history records per proxy for json file
 $max_history_days = 180; //-- max days of graph history
 
 ?>
+
 
